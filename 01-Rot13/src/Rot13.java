348610d496b3d710740006022969cddbb8ac7406
baeimaan <imaanjavaid03@gmail.com>
2026-09-20 14:36:18 +0200

    Implementing, utilitzant desplaçaLletra

--- 01-Rot13/src/Rot13.java
+++ 01-Rot13/src/Rot13.java
@@ -11,12 +11,21 @@ public class Rot13 {
         'j','k','l','m','n','ñ','o','ó','ò','p','q','r','s','t','u','ú','ù',
         'ü','v','w','x','y','z'
     };
+
     public static String xifraRot13(String cadena) {
-        return cadena;
+        StringBuilder resultat = new StringBuilder();
+        for (char c : cadena.toCharArray()) {
+            resultat.append(desplaçaLletra(c, 13));
+        }
+        return resultat.toString();
     }
 
     public static String desxifraRot13(String cadena) {
-        return cadena; 
+        StringBuilder resultat = new StringBuilder();
+        for (char c : cadena.toCharArray()) {
+            resultat.append(desplaçaLletra(c, -13));
+        }
+        return resultat.toString();
     }
 
     private static int indexOf(char[] array, char c) {
